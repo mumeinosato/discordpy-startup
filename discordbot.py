70f476f4882ddb8b5fb9857f614aed28da263090
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-10-08 18:52:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,10 +7,28 @@ from discord.ext import commands
 import os
 import traceback
 import random
+import urllib.request
+import json
+import re
 
 bot = commands.Bot(command_prefix="mus:", help_command=None)
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
 
+citycodes = {
+    "土浦": '080020',
+    "水戸": '080010',
+    "札幌": '016010',
+    "仙台": '040010',
+    "東京": '130010',
+    "横浜": '140010',
+    "名古屋": '230010',
+    "大阪": '270000',
+    "広島": '340010',
+    "福岡": '400010',
+    "鹿児島": '460010',
+    "那覇": '471010'
+}
 
 @bot.event
 async def on_ready():
@@ -83,6 +101,35 @@ async def ping(ctx):
     await msg.edit(content=f"測定結果:{round(ping * 1000)}ms")
     #float(ping * 1000)
 
+@client.event
+async def on_ready():
+  print("logged in as " + client.user.name)
+
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+
+    reg_res = re.compile(u"Bot君、(.+)の天気は？").search(message.content)
+    if reg_res:
+
+      if reg_res.group(1) in citycodes.keys():
+
+        citycode = citycodes[reg_res.group(1)]
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
+        resp = json.loads(resp.decode('utf-8'))
+
+        msg = resp['location']['city']
+        msg += "の天気は、\n"
+        for f in resp['forecasts']:
+          msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+        msg += "です。"
+
+        await client.send_message(message.channel, message.author.mention + msg)
+
+      else:
+        await client.send_message(message.channel, "そこの天気はわかりません...")
+
+    
 @bot.command()
 async def wiki(ctx, *,arg:str=""):
     """
