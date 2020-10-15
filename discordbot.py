4486911474040758404c691d9af95f4b3afc4f27
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-10-15 18:16:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -265,28 +265,32 @@ async def on_message(message):
     if message.channel.name == GLOBAL_CH_NAME:                                                          
                                                            print("success")
                                                            await message.delete() # 元のメッセージは削除しておく
+                                                           channels = client.get_all_channels()
+                                                           global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+                                                           embed = discord.Embed(title="hoge-global",
+                                                               description=message.content, color=0x00bfff)
+                                                           embed.set_author(name=message.author.display_name, 
+            　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　icon_url=message.author.avatar_url_as(format="png"))
+        　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　icon_url=message.guild.icon_url_as(format="png"))# Embedインスタンスを生成、投稿者、投稿場所などの設定
+                                   　　　　　　　　　　　　　　　for channel in global_channels:# メッセージを埋め込み形式で転送
+            　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 await channel.send(embed=embed)
+                                                           
+            
+        # Embedインスタンスを生成、投稿者、投稿場所などの設定
+
+        
+        # channelsはbotの取得できるチャンネルのイテレーター
+        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+
+        
+        
                                                            
         # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
 
                                                            
 
-        channels = client.get_all_channels()
-        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
-        # channelsはbotの取得できるチャンネルのイテレーター
-        # global_channelsは hoge-global の名前を持つチャンネルのリスト
-
-        embed = discord.Embed(title="hoge-global",
-            description=message.content, color=0x00bfff)
-
-        embed.set_author(name=message.author.display_name, 
-            icon_url=message.author.avatar_url_as(format="png"))
-        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
-            icon_url=message.guild.icon_url_as(format="png"))
-        # Embedインスタンスを生成、投稿者、投稿場所などの設定
-
-        for channel in global_channels:
-            # メッセージを埋め込み形式で転送
-            await channel.send(embed=embed)
+        
                                                            
 @bot.command()
 async def wiki(ctx, *,arg:str=""):
