28e9f94e1e65d7cf0ea70efae975bdd596b5aee1
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-10-15 17:28:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -255,6 +255,30 @@ async def on_message(message):
       else:
         await client.send_message(message.channel, "そこの天気はわかりません...")
 
+ @client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    GLOBAL_CH_NAME = "hoge-global" # グローバルチャットのチャンネル名
+    GLOBAL_WEBHOOK_NAME = "hoge-webhook" # グローバルチャットのWebhook名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+        await message.delete()
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+
+        for channel in global_channels:
+            ch_webhooks = await channel.webhooks()
+            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+
+            if webhook is None:
+                # そのチャンネルに hoge-webhook というWebhookは無かったので無視
+                continue
+            await webhook.send(content=message.content,
+                username=message.author.name,
+                avatar_url=message.author.avatar_url_as(format="png"))                                                          
     
 @bot.command()
 async def wiki(ctx, *,arg:str=""):
